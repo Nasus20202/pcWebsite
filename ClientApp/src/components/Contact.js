007b36e50a57d22417af28c4eafe2576bc0627e7
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-08-07 16:29:29 +0200

    Kinda works

--- ClientApp/src/components/Contact.js
+++ ClientApp/src/components/Contact.js
@@ -5,9 +5,30 @@ export class Contact extends Component {
 
   render() {
     return (
-        <div>
-          <h1>Kontakt</h1>
-        </div>
+        [<div className="list-group computer-list list-group-item">
+          <h1 className="d-flex justify-content-center" style={{margin: '20px'}}>Kontakt</h1>
+            <div style={{fontSize: '1.3em', color: '#444'}}>
+                Masz jakieś pytania dotyczące zestawów komputerowych? Żaden z nich nie jest dla Ciebie odpowiedni?
+                Potrzebujesz pomocy z wyborem komponentów? Skontaktuj się ze mną poprzez email na adres <a className="fw-bold" style={{textDecoration: "none", color: "inherit"}} href="mailto:krzysztof.nasuta@wp.pl">krzysztof.nasuta@wp.pl</a>.
+                <div className="mt-3">
+                    Oferuję:
+                    <ul>
+                        <li>Pomoc w doborze komponentów i peryferiów</li>
+                        <li>Wybór najbardziej opłacalnych podzespołów</li>
+                        <li>Indywidualne dopasowanie zestawu</li>
+                        <li>Złożenie jednoski komputerowej</li>
+                    </ul>
+                </div>
+            </div>
+        </div>,
+      <div className="mt-3 list-group computer-list list-group-item">
+          <div style={{fontSize: '1.1em'}}>
+              W przypadku dodatkowych pytań zapraszam do kontaktu:<br/>
+              Email • <a style={{textDecoration: "none", color: "inherit"}} href="mailto:krzysztof.nasuta@wp.pl">krzysztof.nasuta@wp.pl</a><br/>
+              Discord • Nasus#8143<br/><br/>
+              Więcej informacji o mnie dostępnych jest na stronie internetowej: <a target="_blank" className="fw-bold" style={{textDecoration: "none", color: "inherit"}} href="https://knasuta.me">knasuta.me</a>
+          </div>
+      </div>]
     );
   }
 }
--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {UncontrolledCollapse} from "reactstrap";
+import {NavLink, UncontrolledCollapse} from "reactstrap";
 import { Parallax } from 'react-parallax';
+import {Link} from "react-router-dom";
 
 
 export class Home extends Component {
@@ -38,7 +39,7 @@ export class Home extends Component {
                           <td scope="row">{part.category}</td>
                           <th>{part.name}</th>
                           <td>{(part.price/100).toFixed(2)} zł</td>
-                          <td>{part.photo !== "" ? <img className="img-fluid"  style={{maxWidth: '10vw', maxHeight: '10vh'}} src={part.photo} alt={part.name} /> : "" } </td>
+                          <td>{part.photo !== "" ? <img className="img-fluid part-photo" src={part.photo} alt={part.name} /> : "" } </td>
                         </tr>,
                             part.about !== "" ?
                           <tr key={part.partId + "-about"}>
@@ -51,6 +52,7 @@ export class Home extends Component {
                 </UncontrolledCollapse>
               </div>
           )}
+          <Link style={{color: 'inherit', textDecoration: 'none'}} to="/kontakt" className="list-group-item">Jeśli potrzebujesz pomocy w wyborze komponentów lub złożeniu komputera, zapraszam do kontaktu</Link>
         </div>
     );
   }
@@ -62,9 +64,9 @@ export class Home extends Component {
 
     return (
         <div>
-          <div style={{margin: '80px 0 30px 0'}}>
-            <h1 className="d-flex justify-content-center">Polecane komputery</h1>
-            <h5 className="d-flex justify-content-center">Lista polecanych podzespołów komputerowych</h5>
+          <div className="list-group computer-list list-group-item" style={{margin: '80px 0 30px 0'}}>
+            <h1 className="d-flex justify-content-center" style={{fontSize: '3.5em'}}>Polecane komputery</h1>
+            <h5 className="d-flex justify-content-center" style={{fontSize: '1.5em'}}>Lista polecanych zestawów podzespołów komputerowych</h5>
           </div>
             {contents}
         </div>
